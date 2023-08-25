33dbbec44968c8204cf9f4a7e1408aab8037517e
lanrongyi <lanrongyi@mit.cn>
2023-08-25 11:24:21 +0800

    updated README.md

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# dockerfiles
+# 简介
 
+汇总常用的不同环境的Dockerfile在这个仓库。
+
+Airflow未来是否能丝滑运作也有赖于这个仓库的良好维护。
+
+# 编译Docker镜像
+
+### Cyclops环境
+
+```bash
+sh build_cyclops_env.sh
+```
--- build_cyclops_env.sh
+++ build_cyclops_env.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-tag=$1
+# tag=$1
 
-if [ "$#" -ne 1 ]; then
-  echo "Please provide <tag>"
-  echo "Usage: $0 <tag> " >&2
-  exit 1
-fi
+# if [ "$#" -ne 1 ]; then
+#   echo "Please provide <tag>"
+#   echo "Usage: $0 <tag> " >&2
+#   exit 1
+# fi
 
-docker build -f dockerfiles/cyclops-env/Dockerfile -t $tag .
+tag="cyclops-env:cuda11.3-ubuntu20.04"
+echo "Building Docker Image: ${tag}"
+
+docker build -f dockerfiles/cyclops-env/Dockerfile -t cyclops-env:cuda11.3-ubuntu20.04 .
