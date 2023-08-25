c9ef745abe639aa6ee9d7df75d25968fe6809c7b
lanrongyi <lanrongyi@mit.cn>
2023-08-25 19:20:28 +0800

    separate cyclops-env Dockerfile into cu110 and cu113

--- build_cyclops_env.sh
+++ build_cyclops_env.sh
@@ -2,13 +2,23 @@
 
 # tag=$1
 
-# if [ "$#" -ne 1 ]; then
-#   echo "Please provide <tag>"
-#   echo "Usage: $0 <tag> " >&2
-#   exit 1
-# fi
+if [ "$#" -ne 1 ]; then
+  echo "Please provide cuda version: [cu110, cu113]"
+  echo "Usage: $0 <cuda_version> " >&2
+  exit 1
+fi
 
-tag="cyclops-env:cuda11.3-ubuntu20.04"
+cuda_version=$1
+
+if [ "${cuda_version}" = "cu110" ] || [ "${cuda_version}" = "cu113" ]; then
+  echo "target cuda version is ${cuda_version}"
+else
+  echo "Invalid cuda version"
+  exit 1
+fi
+
+tag="cyclops-env:${cuda_version}-ubuntu20.04"
+
 echo "Building Docker Image: ${tag}"
 
-docker build -f dockerfiles/cyclops-env/Dockerfile -t ${tag} .
+docker build -f dockerfiles/cyclops-env/Dockerfile-${cuda_version} -t ${tag} .
